aef314f2b29aebf599d0ed6d27a984198cc03e3b
Chanaka Jayasena <chanaka3d@gmail.com>
2023-04-20 10:23:08 +0530

    Adding the admin UI to the repo

---
+++ admin/admin-ui/client/public/conf/Settings.js
@@ -0,0 +1,38 @@
+
+
+const Settings = {
+    // oath0 config
+    // idp: {
+    //     client_id: 'wqCauPZqUn6UigcAZbU9Z6jxwNTfzOAb',
+    //     well_known: 'https://construct.auth0.com/.well-known/openid-configuration',
+    //     serverOrigin: 'https://construct.auth0.com/',
+    //     redirect_uri: 'https://localhost:4000',
+    //     logout_endpoint: 'https://construct.auth0.com/v2/logout',
+    //     scope: 'openid offline_access',
+    //     state: 'RlZyVjlqYUpHTzltWC42c2FNRDRJT1JPfk1+TUFEa0RLb04yZldwYkpxVA==',
+    //     pkce: true,
+    // },
+    idp: {
+        client_id: '01eddd9e-8425-1e28-8171-19fc60bee195',
+        client_secret: '01eddd9e-8425-1e28-8d62-e604275e23e3',
+        host: 'idp.am.wso2.com:9095',
+        server_origin: 'https://idp.am.wso2.com:9095/',
+        redirect_uri: 'https://localhost:4000',
+        logout_endpoint: 'https://idp.am.wso2.com:9095/logout',
+        scope: 'openid offline_access',
+        state: 'RlZyVjlqYUpHTzltWC42c2FNRDRJT1JPfk1+TUFEa0RLb04yZldwYkpxVA==',
+        authorization_endpoint: 'https://idp.am.wso2.com:9095/oauth2/authorize',
+        token_endpoint: 'https://idp.am.wso2.com:9095/oauth2/token',
+        jwks_uri: 'https://idp.am.wso2.com:9095/oauth2/jwks',
+        issuer: 'https://idp.am.wso2.com:9095/oauth2/token',
+        userinfo_endpoint: 'https://idp.am.wso2.com:9095/oauth2/userinfo',
+        pkce: false,
+    },
+    app: {
+        rest_api: 'https://api.am.wso2.com:9095/api/am/admin',
+    }
+};
+
+if (typeof module !== 'undefined') {
+    module.exports = Settings; // For Jest unit tests
+}
